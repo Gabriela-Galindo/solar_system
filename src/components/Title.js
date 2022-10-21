4d54c173e56f86b1b2601347b15bf215c45d1ae8
Maria Gabriela Galindo <gabi_who@live.cm>
2022-10-21 15:35:31 -0300

    Adiciona requisito 4

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
+    // eslint-disable-next-line react/prop-types
     const { headline } = this.props;
     return (
       <h2>{headline}</h2>
     );
   }
 }
+
+Title.defaultProps = {
+  headline: PropTypes.string,
+};
 
 export default Title;
